59073b090d1c1f73bb5b1b8f2f0f4d3bbbd255b4
Helga09 <olha.vashchenko@advantiss.com>
2022-09-24 20:37:03 +0300

    Add the method and do 3 tests

---
+++ StringCalculator/Calculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator
+{
+    class Calculator
+    {
+        private int CalculateSum(string numbers)
+        {
+            var arrayNumbers = numbers.Split(',').Select(x => int.Parse(x)).ToArray();
+            int sum = 0;
+            for (int i = 0; i < arrayNumbers.Length; i++)
+            {
+                sum += arrayNumbers[i];
+            }
+            return sum;
+        }
+
+        public int Add(string numbers)
+        {
+             if(numbers=="" || numbers == null)
+             {
+                 return 0;
+             }
+             else
+             {
+                return CalculateSum(numbers);
+            }
+        }
+    }
+}
---
+++ StringCalculator/CalculatorTest.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+
+namespace StringCalculator
+{
+    [TestFixture]
+    class CalculatorTest
+    {
+        static Calculator c;
+        [SetUp]
+        public static void Initialize()
+        {
+            c = new Calculator();
+        }
+        [Test]
+        public static void EmptyString()
+        {
+            Assert.AreEqual(0, c.Add(""));
+        }
+        [Test]
+        public static void OneParameter()
+        { 
+            Assert.AreEqual(5, c.Add("5"));
+        }
+        [Test]
+        public static void TwoParameter()
+        {
+            Assert.AreEqual(3, c.Add("1,2"));
+        }
+    }
+}
